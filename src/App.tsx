eea094218f5b2348ce88050eca632b8cc61d3b46
SUMAN KUMAR <KSUMANK80@GMAIL.COM>
2025-05-29 17:57:35 +0530

    Update theme colors to match reference design

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen bg-white text-gray-900 flex flex-col">
+    <div className="min-h-screen bg-gray-900 text-gray-100 flex flex-col">
       <Header />
       <main className="flex-grow">
         <Hero />
