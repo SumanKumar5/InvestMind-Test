e92328afba327b3db20e46993dbc2f96186d51fe
SUMAN KUMAR <KSUMANK80@GMAIL.COM>
2025-05-31 18:06:54 +0530

    Undo Portfolio Details Changes

--- src/types/holding.ts
+++ src/types/holding.ts
@@ -1,24 +1,4 @@
-export interface Holding {
-  id: string;
-  symbol: string;
-  companyName: string;
-  quantity: number;
-  buyPrice: number;
-  currentPrice: number;
-  marketValue: number;
-  gainLoss: number;
-  gainLossPercentage: number;
-  sector: string;
-}
-
-export interface SectorExposure {
-  sector: string;
-  percentage: number;
-}
-
 export interface PortfolioAnalytics {
   totalInvestment: number;
-  currentValue: number;
   profitLossPercentage: number;
-  cagr?: number;
 }
